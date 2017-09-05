ab5c8dcab38dd34d247f663f4ddc7070e45c174e
Ma <yitao.ma@sap.com>
2017-09-05 22:49:46 +0800

    added test.js

---
+++ test.js
@@ -0,0 +1,15 @@
+
+
+console.log(process.env.npm_package_name);
+console.log(process.env.npm_package_dependencies_jquery);
+
+console.log(process.env.npm_config_registry);
+console.log(process.env.npm_package_main);
+console.log(process.env.npm_package_repository);
+
+console.log(process.env.npm_package_config_port);
+console.log(process.env.npm_package_config_tw);
+console.log(process.env.npm_config_tw);
+console.log(process.env.npm_config_demo_port);
+console.log(process.env.npm_config_port);
+
