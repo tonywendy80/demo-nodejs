3c8e07e522f9a81ba64053717221ed5df62b3d76
tony <tonywendy80@qq.com>
2020-05-11 18:34:25 +0800

    added more traces

--- test.js
+++ test.js
@@ -2,8 +2,12 @@
 var http = require("http");
 
 http.createServer(function(req, rsp) {
+	console.log("received http request...");
+
     rsp.writeHead(200, "Content-Type:text/plain");
     rsp.end("hello world!");
+
+    console.log("sent back http response 200!");
 }).listen(8888);
 
 
